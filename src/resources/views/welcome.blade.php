892a86de9124051200113a04ff8fd1c80b51d6fe
shunsock <shunsuke.tsuchiya@prtimes.co.jp>
2023-07-10 15:04:55 +0900

    :wrench: [update configuration files] =>  .ideaを削除しました

--- src/resources/views/welcome.blade.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    @include('components.head')
-    <div>
-        <h1 class="title">NEWSORT TIMES</h1>
-    </div>
-</html>
