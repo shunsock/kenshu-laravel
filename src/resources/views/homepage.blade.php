dcf5b1a8055909a4aa4aba845e65a8a55114379d
shunsock <shunsuke.tsuchiya@prtimes.co.jp>
2023-07-11 12:16:34 +0900

    :lipstick: [update ui] =>  記事を表示できるようにしました

--- src/resources/views/homepage.blade.php
+++ src/resources/views/homepage.blade.php
@@ -2,4 +2,5 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     @include('components.head')
     @include('components.header')
+    @include('homepage.articleTable', ['articles' => $articles])
 </html>
