c01b72f994a10987ffd42c30458d0950aa0aa512
shunsock <shunsuke.tsuchiya@prtimes.co.jp>
2023-07-12 13:16:21 +0900

    :goal_net: [catch errors] =>  トップページでのエラーハンドリングを追加しました

--- src/app/Http/Controllers/showArticleController.php
+++ src/app/Http/Controllers/showArticleController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 use App\Service\SupplyArticle;
 use App\Service\Message;
+use Dotenv\Exception\InvalidFileException;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -21,13 +22,20 @@ class showArticleController extends Controller
         try {
             $articles = SupplyArticle::articleData();
             $message = Message::extract();
+        } catch (InvalidFileException) {
+            return view(
+                view: 'homepage'
+                , data: [
+                    'message' => 'Internal Server Error: InvalidFileException'
+                ]
+            );
         } catch (InvalidArgumentException) {
             return view(
                 view: 'homepage'
                 , data: [
                     'message' => 'Internal Server Error: InvalidArgumentException'
                 ]
-            )  ;
+            );
         } catch (PDOException){
             return view(
                 view: 'homepage'
