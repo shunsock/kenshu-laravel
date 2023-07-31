93a2956d9f2f308dac6de22a77515362d7d8d942
shunsock <shunsuke.tsuchiya@prtimes.co.jp>
2023-07-31 15:45:17 +0900

    :safety_vast: [update validation code] =>  deleteとeditで使用するmiddlewareを作成しました

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'checkusername' => \App\Http\Middleware\CheckUsername::class,
+        'checkifeditorisauthor' => \App\Http\Middleware\CheckIfEditorIsAuthor::class,
     ];
 }
